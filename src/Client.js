bca9ac9c01e132ba3accb1208c0aaa8931a9c057
John Paraskevopoulos <io.paraskev@gmail.com>
2018-09-22 16:45:17 +0300

    Add Client algorithm options fetchers
    - Adds functions to fetch supported algorithm options from backend and
    to fetch the default values that the server uses

--- src/Client.js
+++ src/Client.js
@@ -28,6 +28,32 @@ function sendEncryptRequest(data, cb) {
     .catch(cb);
 }
 
+function fetchAlgorithmOptions(cb) {
+  return fetch("/config/algorithms/")
+    .then(checkStatusOk)
+    .then(parseJSON)
+    .then((r) => cb({}, r))
+    .catch(cb);
+}
+
+function fetchDefaultAlgorithmOptions(cb) {
+  return fetch('/config/algorithms/default')
+    .then(checkStatusOk)
+    .then(parseJSON)
+    .then((r) => cb({}, r))
+    .catch(cb);
+}
+
+
+function checkStatusOk(response) {
+  if (response.status === 200) {
+    return response;
+  }
+  const error = new Error(`HTTP Error ${response.statusText}`);
+  error.status = response.statusText;
+  error.response = response;
+  throw error;
+}
 
 function checkStatus(response) {
   if ([200, 400, 404].indexOf(response.status) !== -1) {
@@ -44,5 +70,6 @@ function parseJSON(response) {
 }
 
 
-const Client = { sendDecryptionRequest, sendEncryptRequest };
+const Client = { sendDecryptionRequest, sendEncryptRequest,
+  fetchAlgorithmOptions, fetchDefaultAlgorithmOptions };
 export default Client;
